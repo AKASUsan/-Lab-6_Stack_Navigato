b91f32901875898274c91137e2a6d7ecb239fdf6
AKASUsan <kasubaranime@gmail.com>
2026-08-25 21:17:08 +0700

    App.js

--- App.js
+++ App.js
@@ -1,24 +1,92 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet} from "react-native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
 import CategoriesScreen from "./screens/CategoryScreen";
-import MealsOverviewScreen from "./screens/MealsOverviewScreen"
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
+import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import FavoritesContextProvider from "./store/context/favorites-context";
+import { Ionicons } from "@expo/vector-icons";
+
 const Stack = createNativeStackNavigator();
-export default function App() {
-return (
-<>
-<StatusBar style="auto" />
-{/* <CategoriesScreen /> */}
-<NavigationContainer>
-<Stack.Navigator>
-<Stack.Screen name="Kanthachart Kopimai 6714110029" component={CategoriesScreen} />
-<Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-</Stack.Navigator>
-</NavigationContainer>
-</>
-);
+const Drawer = createDrawerNavigator();
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="FavoriteMealScreen"
+        component={FavoritesScreen}
+        options={{
+            title:"Kanthachart Kopimai 6714110029",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
 }
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <FavoritesContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Drawer"
+            screenOptions={{
+              headerStyle: { backgroundColor: "#351401" },
+              headerTintColor: "white",
+              contentStyle: { backgroundColor: "#3f2f25" },
+            }}
+          >
+            <Stack.Screen
+              name="Drawer"
+              component={DrawerNavigator}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MealsCategories"
+              component={CategoriesScreen}
+              options={{ title: "All Categories" }}
+            />
+            <Stack.Screen
+              name="MealsOverview"
+              component={MealsOverviewScreen}
+            />
+            <Stack.Screen
+              name="MealDetails"
+              component={MealDetailScreen}
+              options={{ title: "About the Meal" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FavoritesContextProvider>
+    </>
+  );
+}
+
 const styles = StyleSheet.create({
-container: {},
+  container: {},
 });
