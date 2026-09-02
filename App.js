165d5323264765e58c7a4fb067f238a7e082085f
AKASUsan <kasubaranime@gmail.com>
2026-09-02 16:39:15 +0700

    Add BottomTag

--- App.js
+++ App.js
@@ -4,16 +4,65 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import CategoriesScreen from "./screens/CategoryScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 import FavoritesScreen from "./screens/FavoritesScreen";
+import OrdersScreen from "./screens/OrdersScreen";
 import FavoritesContextProvider from "./store/context/favorites-context";
 import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function BottomTabNavigator() {
+  return (
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: "#3f2f25" },
+        tabBarActiveTintColor: "white",
+      }}
+    >
+      <BottomTabs.Screen
+        name="All Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          title: "Favorites",
+          tabBarLabel: "Favorites",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="star" size={size} color={color} />
+          ),
+        }}
+      />
+       <BottomTabs.Screen
+        name="Orders"
+        component={OrdersScreen}
+        options={{
+          title: "Orders",
+          tabBarLabel: "Orders",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="cart" size={size} color={color} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
+  );
+}
 function DrawerNavigator() {
   return (
     <Drawer.Navigator
@@ -37,7 +86,7 @@ function DrawerNavigator() {
         name="FavoriteMealScreen"
         component={FavoritesScreen}
         options={{
-            title:"Kanthachart Kopimai 6714110029",
+          title: "Kanthachart Kopimai 6714110029",
           drawerIcon: ({ color, size }) => (
             <Ionicons name="star" color={color} size={size} />
           ),
@@ -54,7 +103,6 @@ export default function App() {
       <FavoritesContextProvider>
         <NavigationContainer>
           <Stack.Navigator
-            initialRouteName="Drawer"
             screenOptions={{
               headerStyle: { backgroundColor: "#351401" },
               headerTintColor: "white",
@@ -62,14 +110,11 @@ export default function App() {
             }}
           >
             <Stack.Screen
-              name="Drawer"
-              component={DrawerNavigator}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="MealsCategories"
-              component={CategoriesScreen}
-              options={{ title: "All Categories" }}
+              name="Categories"
+              component={BottomTabNavigator}
+              options={{
+                headerShown: false,
+              }}
             />
             <Stack.Screen
               name="MealsOverview"
@@ -78,7 +123,9 @@ export default function App() {
             <Stack.Screen
               name="MealDetails"
               component={MealDetailScreen}
-              options={{ title: "About the Meal" }}
+              options={{
+                title: "About the Meal",
+              }}
             />
           </Stack.Navigator>
         </NavigationContainer>
