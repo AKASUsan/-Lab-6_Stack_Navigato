c485b21739820b6d57ff3c63ee70c88873f27b6f
AKASUsan <kasubaranime@gmail.com>
2026-09-02 16:41:25 +0700

    Add background

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -51,14 +51,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+    backgroundColor: "#3f2f25",
   },
   rootContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#3f2f25",
   },
   text: {
     color: "white",
-    fontSize: 16,
   },
 });
